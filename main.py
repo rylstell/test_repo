8052ad1c7886f0b7ee11abfd9403c68b3f215691
Ryan Stella <ryan.stella@uic.com>
2023-09-06 13:31:09 -0400

    Branch local/develop. Added divide function to main.py

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ def sub(num1, num2):
 def multiply(num1, num2):
 	return num1 * num2
 
+def divide(num1, num2):
+	return num1 / num2
+
 def main():
 
 	a = 4
@@ -18,10 +21,12 @@ def main():
 	c = add(a, b)
 	d = sub(a, b)
 	e = multiply(a, b)
+	f = divide(a, b)
 
 	print(c)
 	print(d)
 	print(e)
+	print(f)
 
 if __name__ == "__main__":
 	main()
