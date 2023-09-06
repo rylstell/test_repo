9f40fc12c458bac63939b8e4c8925dfc5ae6e4e0
Ryan Stella <ryan.stella@uic.com>
2023-09-06 10:47:13 -0400

    added multiply function on develop branch

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ def add(num1, num2):
 def sub(num1, num2):
 	return num1 - num2
 
+def multiply(num1, num2):
+	return num1 * num2
+
 def main():
 
 	a = 4
@@ -12,9 +15,11 @@ def main():
 
 	c = add(a, b)
 	d = sub(a, b)
+	e = multiply(a, b)
 
 	print(c)
 	print(d)
+	print(e)
 
 if __name__ == "__main__":
 	main()
